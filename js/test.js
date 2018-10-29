1b5a0edfbf57322b89859e4383919a0a21960de0
getHard1226 <hi5.nakash1ma@gmail.com>
2018-10-29 21:45:15 +0900

    checkout -b 名前でブランチ作成

--- js/test.js
+++ js/test.js
@@ -1,43 +1,10 @@
-var nameFlg = false;
-var pwdFlg = false;
-var buttonFlg = false;
+window.addEventListener('load', function() {
+	init();
+});
 
-function nCheck() {
-	var userName = document.getElementById('userName').value;
-	var userNameLen = userName.length;
-
-	if (userNameLen > 0 ) {
-		nameFlg = true;
-	} else {
-		nameFlg = false;
-	}
-
-	judge();
+function init() {
+	document.getElementById('firstReel').innerHTML = '7';
+	document.getElementById('secondReel').innerHTML = '7';
+	document.getElementById('thirdReel').innerHTML = '7';
 }
-
-function pCheck() {
-	var userPwd = document.getElementById('pwd').value;
-	var userPwdLen = userPwd.length;
-
-	if (userPwdLen > 0 ) {
-		pwdFlg = true;
-	} else {
-		pwdFlg = false;
-	}
-
-	judge();
-}
-
-function judge() {
-	if (nameFlg && pwdFlg ) {
-		buttonFlg = true;
-	} else {
-		buttonFlg = false;
-	}
-
-	if (buttonFlg) {
-		document.getElementById('next').disabled = false;
-	} else {
-		document.getElementById('next').disabled = true;
-	}
-}
+		
