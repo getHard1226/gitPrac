f2d4e0e65f2f25c02cfbfff0ac160ff6272a6cca
getHard1226 <hi5.nakash1ma@gmail.com>
2018-10-31 21:33:20 +0900

    簡単なgogo機能

--- js/test.js
+++ js/test.js
@@ -2,6 +2,8 @@ window.addEventListener('load', function() {
 	init();
 });
 
+var DENOMI = 10;
+
 function init() {
 	document.getElementById('firstReel').innerHTML = '7';
 	document.getElementById('secondReel').innerHTML = '7';
@@ -9,11 +11,24 @@ function init() {
 }
 
 var first, second, third;
+var MAX_VALUE = 8;
+var credit = 50;
 
 function start() {
 	first = setInterval(rool1, 1);
 	second = setInterval(rool2, 1);
 	third = setInterval(rool3, 1);
+	subtraction();
+	lottery();
+}
+
+function lottery() {
+	var i = Math.floor(Math.random() * DENOMI);
+	if (i == 1) {
+		document.getElementById('gogo').innerHTML = 'gogo';
+	} else {
+		document.getElementById('gogo').innerHTML = '';
+	}
 }
 
 
@@ -29,15 +44,24 @@ function stop(arg) {
 
 function rool1() {
 	document.getElementById('firstReel').innerHTML
-		= Math.floor(Math.random() * 8);
+		= Math.floor(Math.random() * MAX_VALUE);
 }
 
 function rool2() {
 	document.getElementById('secondReel').innerHTML
-		= Math.floor(Math.random() * 8);
+		= Math.floor(Math.random() * MAX_VALUE);
 }
 
 function rool3() {
 	document.getElementById('thirdReel').innerHTML
-		= Math.floor(Math.random() * 8);
+		= Math.floor(Math.random() * MAX_VALUE);
+}
+
+function subtraction() {
+	credit = credit - 3;
+	if (credit < 2) {
+		credit = 50;
+	}
+	document.getElementById('credit').innerHTML
+		= credit;
 }
